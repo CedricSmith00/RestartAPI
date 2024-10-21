0b0f255453a4da47d87dfa87f6ff1c81c6f878a0
Jay <179924362+CedricSmith00@users.noreply.github.com>
2024-10-21 15:01:33 +0100

    addUser controller added and route

--- src/server.js
+++ src/server.js
@@ -1,15 +1,25 @@
 const cors = require("cors")
 const express = require("express");
 const app = express();
+const User = require("./db/models/userModel");
+const userRouter = require("./db/routes/userRoutes");
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
+function syncTables() {
+    User.sync({alter:true})
+};
+
 const port = process.env.PORT || 5001;
 
-const SQLconnection = require("./db/connection")
+app.use(userRouter)
+
+// const SQLconnection = require("./db/connection")
 
 app.get("/health", (req,res) => res.status(200).send("API is healthy"));
+
+syncTables();
 
 app.listen(port, () => {console.log(`Server is running on port ${port}`)})
